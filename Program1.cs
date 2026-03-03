f7098a5861094a09718e96b46d2717c20d626851
ilgaram-bpa202-ctrl <ilgaram-bpa202@code.edu.az>
2026-03-03 17:29:42 +0400

    first

---
+++ Program1.cs
@@ -0,0 +1,13 @@
+﻿string birMehsulAdi = "ad1";
+string ikiMehsulAdi = "ad1";
+string ucMehsulAdi = "ad1";
+
+int birMehsulIlkinQIYMET = 100;
+int ikiMehsullkinQIYMET = 200;
+int ucMehsullkinQIYMET = 300;
+
+int endirimFaizi = 10;
+
+int yekunQiymetBir = birMehsulIlkinQIYMET / 100 * 100 - endirimFaizi;
+Console.WriteLine(yekunQiymetBir);
+
