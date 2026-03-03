1289afeeffafc9de1c7a2031bc396258076822a4
ilgaram-bpa202-ctrl <ilgaram-bpa202@code.edu.az>
2026-03-03 17:12:14 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Task 2
+//int a = 12;
+//int b = 5;
+//Console.WriteLine($"{a + b} {a - b} {a * b} {a % b}");
+
+
+
+//Task3
+//int first = 1200;
+//int second = 1500;
+//int third = 900;
+//int fourth = 2000;
+
+//int cem = first + second + third + fourth;
+//int ortalama = cem / 4;
+//Console.WriteLine(ortalama);
+
+
+
+//Task4
+//int birinciTeref = 6;
+//int ikinciTeref = 8;
+//int ucuncuTeref = 10;
+//int sahe = (birinciTeref * ikinciTeref) / 2;
+//Console.WriteLine(sahe);
